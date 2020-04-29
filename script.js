155548638ebc6924a5cc25002ba5ce60f188a964
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-29 04:45:02 -0700

    commit 3

--- script.js
+++ script.js
@@ -42,3 +42,56 @@ function pauseSong() {
 
   audio.pause();
 }
+
+// Previous song
+function prevSong() {
+    songIndex--;
+  
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+  
+    loadSong(songs[songIndex]);
+  
+    playSong();
+  }
+  
+  // Next song
+  function nextSong() {
+    songIndex++;
+  
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+  
+    loadSong(songs[songIndex]);
+  
+    playSong();
+  }
+  
+  // Update progress bar
+  function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+  
+  // Set progress bar
+  function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+  
+    audio.currentTime = (clickX / width) * duration;
+  }
+  
+  // Event listeners
+  playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+  
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  });
